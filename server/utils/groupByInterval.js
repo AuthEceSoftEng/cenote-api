7aacff8652c5b77cfcee01c38074a3766f7f48cb
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2020-01-07 16:50:10 +0200

    Minor bug fix

--- server/utils/groupByInterval.js
+++ server/utils/groupByInterval.js
@@ -10,22 +10,22 @@ module.exports = (items, interval, type, target = '', p = 100) => {
   const grouped = {};
   items.forEach((value) => {
     let d = moment(value.cenote$timestamp);
-    if (interval === 'minutely') d = d.minute();
-    if (interval === 'hourly') d = d.hour();
-    if (interval === 'daily') d = d.dayOfYear();
-    if (interval === 'weekly') d = d.weekYear();
-    if (interval === 'monthly') d = d.month();
-    if (interval === 'yearly') d = d.year();
+    if (interval === 'minutely') d = d.startOf('minute').toISOString();
+    if (interval === 'hourly') d = d.startOf('hour').toISOString();
+    if (interval === 'daily') d = d.startOf('day').toISOString();
+    if (interval === 'weekly') d = d.startOf('week').toISOString();
+    if (interval === 'monthly') d = d.startOf('month').toISOString();
+    if (interval === 'yearly') d = d.startOf('year').toISOString();
     grouped[d] = grouped[d] || [];
     grouped[d].push(value);
   });
   Object.keys(grouped).forEach((val) => {
-    if (interval === 'minutely') results.push({ interval: moment().minute(val).format('DD-MMM-YYYY:HH:mm'), result: grouped[val] });
-    if (interval === 'hourly') results.push({ interval: moment().hour(val).minute(0).format('DD-MMM-YYYY:HH:mm'), result: grouped[val] });
-    if (interval === 'daily') results.push({ interval: moment().dayOfYear(val).format('DD-MMM-YYYY'), result: grouped[val] });
-    if (interval === 'weekly') results.push({ interval: moment().weekYear(val).format('DD-MMM-YYYY'), result: grouped[val] });
-    if (interval === 'monthly') results.push({ interval: moment().month(val).format('MMM-YYYY'), result: grouped[val] });
-    if (interval === 'yearly') results.push({ interval: moment().year(val).format('YYYY'), result: grouped[val].length });
+    if (interval === 'minutely') results.push({ interval: moment(val).format('DD-MMM-YYYY:HH:mm'), result: grouped[val] });
+    if (interval === 'hourly') results.push({ interval: moment(val).minute(0).format('DD-MMM-YYYY:HH:mm'), result: grouped[val] });
+    if (interval === 'daily') results.push({ interval: moment(val).format('DD-MMM-YYYY'), result: grouped[val] });
+    if (interval === 'weekly') results.push({ interval: moment(val).format('DD-MMM-YYYY'), result: grouped[val] });
+    if (interval === 'monthly') results.push({ interval: moment(val).format('MMM-YYYY'), result: grouped[val] });
+    if (interval === 'yearly') results.push({ interval: moment(val).format('YYYY'), result: grouped[val].length });
   });
   if (type === 'count') return results.map(el => ({ ...el, result: el.result.length }));
   if (type === 'minimum') return results.map(el => ({ ...el, result: Math.min(...el.result.map(ele => ele[target])) }));
