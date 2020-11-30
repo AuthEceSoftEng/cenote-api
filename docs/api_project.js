e0f072af7f87f4b33b062223948bfbde1c819839
Giannis Antoniadis <johneegr@gmail.com>
2020-12-01 01:24:56 +0200

    Updated eslint to 6.8.0 #42

--- docs/api_project.js
+++ docs/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-11-21T19:31:15.849Z",
+    "time": "2020-11-30T23:25:28.077Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
