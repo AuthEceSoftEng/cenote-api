fdf7798cefd0212a7cc0b430fa2f6a6712e58225
Giannis Antoniadis <johneegr@gmail.com>
2020-12-01 17:12:04 +0200

    Update dependencies of yargs-parser for security risk pt.2 #42

--- docs/api_project.js
+++ docs/api_project.js
@@ -14,7 +14,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-01T15:10:20.920Z",
+    "time": "2020-12-01T15:12:16.575Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
